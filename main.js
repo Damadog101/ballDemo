dd5177c91d5a8808e7fbf5084c3f8b483b8b6269
Ethan <116187182+Damadog101@users.noreply.github.com>
2023-05-18 12:10:52 -0700

    Update main.js

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ Ball.prototype.update = function() {
 let balls = [];
 
 while (balls.length < 10) {
-  let size = 50;
+  let size = 100;
   let ball = new Ball(
    
     random(0 + size,width - size),
